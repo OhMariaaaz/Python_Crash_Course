2ec06cbbf97397ed9b8cf091bf06a8731e880c09
Maria Eduarda Gomes <98362904+OhMariaaaz@users.noreply.github.com>
2024-05-16 12:51:33 -0300

    Page 80

--- src/Chapter3/friendsMessage.py
+++ src/Chapter3/friendsMessage.py
@@ -1,8 +1,6 @@
-
-
+# Exercises 3.1, 3.2, and 3.3
 pokemons = ['Bulbasaur','Charmander','Squirtle','Pikachu']
 for pokemon in pokemons:
-
     print( pokemon + "! I choose you!!!")
 
 
---
+++ src/Chapter3/myParty.py
@@ -0,0 +1,29 @@
+# Exercises 3.4, 3.5, 3.6 and 3.7 
+party_friends = ["Ros'Galthar","TT","Hofio","Santo Ernesto","Percival","Geslav","L30"]
+print(party_friends(6) + "was removed from the server.")
+party.friends[6] = "Mikha"
+
+for i in party_friends:
+    print(i + "! Welcome to the server!")
+
+print("Now we have a bigger session! I convited more characters to join or campaign.")
+new_members = ["Dra. Choo","Glitch","Avi","Whisky"]
+party_friends.insert(0,new.members[0])
+party_friends.insert(2,new.members[1])
+party_friends.append(new.members[2])
+party_friends.append(new.members[3])
+
+for i in party_friends:
+    print(i + "! Welcome to the server!")
+
+print(party_friends.len())
+
+while party_friends.len() > 3:
+    popped = party_friends.pop()
+    print(popped + ", I'm so sorry... But your character died. Good bye and good luck!")
+
+for i in party_friends:
+    print(i + " has achieved a new level!")
+
+del party_friends(0)
+del party_friends(0)
