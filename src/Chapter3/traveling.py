a9e12291450f05cf782f64832d452a1aefd27ebf
Maria Eduarda Gomes <98362904+OhMariaaaz@users.noreply.github.com>
2024-05-22 20:49:03 -0300

    Chapter 99

---
+++ src/Chapter3/traveling.py
@@ -0,0 +1,13 @@
+# Exercise 3.8
+travel_list = ['Tokyo','Mexico','German','Auriga','Russia']
+print("Normal list: " + str(travel_list))
+print("Sorted list: " + str(sorted(travel_list)))
+print("Normal list: " + str(travel_list))
+travel_list.reverse()
+print("Reversed list: " + str(travel_list))
+travel_list.reverse()
+print("Normal list: " + str(travel_list))
+travel_list.sort()
+print("Sorted list(again): " + str(travel_list))
+travel_list.sort(reverse=True)
+print("Sorted list(again and reversed): " + str(travel_list))
---
+++ src/Chapter4/myDiet.py
@@ -0,0 +1,13 @@
+# Exercises 4.1 and 4.2
+
+print("I have to be more health, and I'll gona start by my food!!!")
+foods = ['Salad','Chicken','Egg','Rice and Beans','Fruits','Vegetables']
+for food in foods:
+    print("I have to eat " + str(food))
+print("This way, I'll get more healthier!")
+
+sports = ['Dance','Jump','Run']
+print("And I have to do sports, like...")
+for sport in sports:
+     print(sport)
+print(" Too!!!")
