0e804c1982939388df9556c14c6e160d7a0c5106
Maria Eduarda Gomes <98362904+OhMariaaaz@users.noreply.github.com>
2024-05-28 06:47:19 -0300

    Page 109

--- src/Chapter4/myDiet.py
+++ src/Chapter4/myDiet.py
@@ -1,8 +1,10 @@
-# Exercises 4.1 and 4.2
+# Exercises 4.1, 4.2 and 4.10
 
 print("I have to be more health, and I'll gona start by my food!!!")
-foods = ['Salad','Chicken','Egg','Rice and Beans','Fruits','Vegetables']
-for food in foods:
+my_foods = ['Salad','Chicken','Egg','Rice and Beans','Fruits','Vegetables']
+
+'''
+for food in my_foods:
     print("I have to eat " + str(food))
 print("This way, I'll get more healthier!")
 
@@ -11,3 +13,24 @@ print("And I have to do sports, like...")
 for sport in sports:
      print(sport)
 print(" Too!!!")
+'''
+
+'''
+print(str(my_foods[:3]) + " is in the beggining of the list.")
+print(str(my_foods[3:4]) + " is in the middle of the list.")
+print(str(my_foods[-2:]) + " is in the end of the list.")
+'''
+
+husband_foods = my_foods[:]
+my_foods.append('Gelatin')
+husband_foods.append('Diet Coke')
+
+'''
+print("That's what I eat:")
+for food in my_foods:
+    print(food)
+    
+print("That's what my husband eat:")
+for food in husband_foods:
+    print(food)
+'''
