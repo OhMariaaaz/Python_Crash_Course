fb5d470225cc601060051ace3f42b785fbb30ab6
Maria Eduarda Gomes <98362904+OhMariaaaz@users.noreply.github.com>
2024-05-28 02:05:23 -0300

    Page 105

---
+++ src/Chapter4/listComprehension.py
@@ -0,0 +1,35 @@
+# Exercises 4.3, 4.4, 4.5, 4.6, 4.7, 4.8 and 4.9
+
+'''
+numbers_20 = []
+for number in range(1,21):
+    print(number)
+    numbers_20.append(number)
+'''
+
+'''
+million_numbers = list(range(1,1000001))
+for number in million_numbers:
+    print(number)
+    
+print(max(million_numbers))
+print(min(million_numbers))
+print(sum(million_numbers))
+'''
+'''
+odd_number = list(range(1,21,2))
+for number in odd_number:
+    print(number)
+'''
+
+'''
+three_multiplies = list(range(3,31,3))
+for number in three_multiplies:
+    print(number)
+'''
+
+'''
+cubed_numbers = [number**3 for number in range(1,11)]
+for number in cubed_numbers:
+    print(number)
+'''
