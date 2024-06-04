246d609cf49dd4ba3755810338c256228b6b4b52
Maria Eduarda Gomes <98362904+OhMariaaaz@users.noreply.github.com>
2024-06-04 00:01:59 -0300

    Page 115

--- src/Chapter4/buffet.py
+++ src/Chapter4/buffet.py
@@ -1,8 +1,13 @@
+# Exercise 4.13
+
 brazilianMenu = ("Feijoada","Strogonoff","Lasanha","Panqueca","Baião de Dois")
 
 for item in brazilianMenu:
     print(item)
 
-# brazilianMenu(1) = "Pastel"
+# brazilianMenu[1] = "Pastel"
 
 brazilianMenu = ("Feijoada","Pastel","Lasanha","Acarajé","Baião de Dois")
+
+for item in brazilianMenu:
+    print(item)
