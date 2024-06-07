24b11e81cf738b327552e193954663fb0fbc9f34
Maria Eduarda Gomes <98362904+OhMariaaaz@users.noreply.github.com>
2024-06-06 23:20:56 -0300

    Page 132

---
+++ src/Chapter5/colorfulAlien.py
@@ -0,0 +1,12 @@
+# Exercise 5.3, 5.4, and 5.5
+
+alien_color = 'YeLLoW'
+
+if(alien_color.lower() == 'green'):
+    print("You have earned 5 points!")
+elif (alien_color.lower() == 'yellow'):
+    print("You have earned 10 points!")
+elif (alien_color.lower() == 'red'):
+    print("You have earned 15 points!")
+else:
+    print("Invalid color :/")
---
+++ src/Chapter5/lifeStage.py
@@ -0,0 +1,16 @@
+# Exercise 5.6
+
+age = -50
+
+if(age < 2):
+    print("You're a baby!")
+elif(age >= 2 and age < 8):
+    print("You're a child!")
+elif(age >= 8 and age < 13):
+    print("You are a pre-teen")
+elif(age >= 13 and age < 20):
+    print("You are a teen")
+elif(age >= 20 and age < 65):
+    print("You are an adult")
+elif(age >= 65):
+    print("You are an elderly")
