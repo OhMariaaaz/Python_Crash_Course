882ea23e9e7bc7c9f6873c5a641f5364cb1863fa
Maria Eduarda Gomes <98362904+OhMariaaaz@users.noreply.github.com>
2024-06-26 21:20:35 -0300

    Page 149

---
+++ src/Chapter6/dictionary.py
@@ -0,0 +1,11 @@
+# Exercises 6.3
+
+new_words = {
+    'API':'The API is responsable to made connectin between system and the user.',
+    'DevOps': 'Culture and methods that conect operational and development processes,'
+    'focused on productive results and security.',
+    'Featured Flags': 'A tool to do tests and updates in real time, and using real persons'
+    }
+
+for word, meaning in new_words.items():
+    print(word + "\n" + meaning + "\n")
---
+++ src/Chapter6/dndCharacter.py
@@ -0,0 +1,15 @@
+# Exercises 6.1 and 6.2
+
+my_character = {
+    'name':"Geslav",
+    'race':"elven",
+    'age':548
+    }
+# print(my_character['age'])
+
+party_characters = {
+    'Rodrigo':'Rosgalthar',
+    'Goes': 'Hofio',
+    'Kaio': 'Tan',
+    }
+
