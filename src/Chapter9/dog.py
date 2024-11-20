bcc1f3e276c98b9ceb6e934260b9ac291ab7fd92
Maria Eduarda Gomes <marig.gomes168@gmail.com>
2024-11-20 15:20:07 -0300

    Page 205

--- src/Chapter9/dog.py
+++ src/Chapter9/dog.py
@@ -1,16 +1,15 @@
-class Dog():  
-    """Uma tentativa simples de modelar um cachorro."""
+class Dog():
+    """A simple attempt to model a dog."""
 
-    def __init__(self, name, age): 
-        """Inicializa os atributos name e age."""
-        self.name = name 
+    def __init__(self, name, age):
+        """Initialize the attributes name and age."""
+        self.name = name
         self.age = age
         
-    def sit(self): 
-        """Simula um cachorro sentando em resposta a um comando."""
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
         print(self.name.title() + " is now sitting.")
 
-    def roll_over(self): 
-        """Simula um cachorro rolando em resposta a um comando."""
+    def roll_over(self):
+        """Simulate a dog rolling over in response to a command."""
         print(self.name.title() + " rolled over!")
-
---
+++ src/Chapter9/restaurant.py
@@ -0,0 +1,9 @@
+class restaurant(): 
+    '''Creating a restaurant object!'''
+    
+    def __init__(self, restaurant_name, cuisine_type):
+        
+    def open_restaurant(self):
+        
+    def describe_restaurant(self):
+        
